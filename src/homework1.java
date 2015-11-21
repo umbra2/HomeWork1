d178ac565cf44db2accfda18afff08030bdf3917
Dmitriy <umbra2@mail.ru>
2015-11-21 17:18:35 +0300

    Добавление коментариев

--- src/homework1.java
+++ src/homework1.java
@@ -4,12 +4,20 @@ public class homework1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		//Инициализация массивов
 		int[] arr1 = new int[10];
 		int[] arr2 = new int[10];
+		
+		//Заполнение массивов
 		array.randArray(arr1);
 		array.randArray(arr2);
+		
+		//Сортировка массивов
 		array.bblSort(arr1);
 		array.selSort(arr2);
+		
+		//Вывод массивов на экран
 		array.printArray(arr1);
 		array.printArray(arr2);
 		
