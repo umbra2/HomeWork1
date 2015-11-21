14012d2c664f191804fe9a2c912dcbfba686e194
Dmitriy <umbra2@mail.ru>
2015-11-21 15:58:42 +0300

    Добавление сортировки выбором

--- src/array.java
+++ src/array.java
@@ -1,13 +1,15 @@
 
 public class array {
+	
+	//Заполнение массива случайными числами
 	public static void randArray(int inputArray[]){
 		for(int i = 0; i < inputArray.length; i++) {
 			inputArray[i] = (int)(Math.random() * 100);
 		}
 	}
 	
+	//Сортировка методом пузырька
 	public static void bblSort(int inputArray[]) {
-		//Создаем 2 цикла - внешний проводит итеррацию по массиву, внутренний меняет местами элементы в неупорядоченной паре
 		for(int i = inputArray.length - 1; i > 0 ; i--){
 			for(int j = 0; j < i; j++){
 				if (inputArray[j] > inputArray[j + 1]) {
@@ -19,6 +21,27 @@ public class array {
 		}
 		
 	}
+	
+	//Сортировка мотодом выбора
+	public static void selSort(int inputArray[]){
+		for(int i = 0; i < inputArray.length; i++){
+			int min = inputArray[i];
+			int min_i = i;
+			for(int j = i + 1; j < inputArray.length; j++){
+				if(min > inputArray[j]){
+					min = inputArray[j];
+					min_i = j;
+				}
+			}
+			if(i != min_i){
+				int tmp = inputArray[i];
+				inputArray[i] = inputArray[min_i];
+				inputArray[min_i] = tmp;
+			}
+		}
+	}
+	
+	//Вывод массива на экран
 	public static void printArray(int inputArray[]){
 		for (int i = 0; i < inputArray.length; i++){
 			System.out.println(inputArray[i]);
--- src/homework1.java
+++ src/homework1.java
@@ -6,9 +6,19 @@ public class homework1 {
 	 */
 	public static void main(String[] args) {
 		int[] arr1 = new int[10];
-		array.randArray(arr1);
+		int[] arr2 = new int[10];
+		int[] arr3 = new int[10];
+		int[] arr4 = new int[10];
+		//array.randArray(arr1);
+		array.randArray(arr2);
+		//array.randArray(arr3);
+		//array.randArray(arr4);
 		array.bblSort(arr1);
-		array.printArray(arr1);
+		array.selSort(arr2);
+		//array.printArray(arr1);
+		array.printArray(arr2);
+		//array.printArray(arr3);
+		//array.printArray(arr4);
 
 	}
 	
